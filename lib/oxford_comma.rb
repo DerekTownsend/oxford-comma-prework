f445ecfe0f372a504817c34ac992b3e334d3990c
Derek Townsend <derekjavon2@gmail.com>
2019-05-08 04:42:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if array.length==1
     string+=array.join
   elsif array.length==2
-    string+="#{item} "
+    string+=array.join(" and ")
 
   array.each do |item|
     elsif array.last==item
