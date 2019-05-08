4c565c7cab9df8353392cb0392fb0312df889923
Derek Townsend <derekjavon2@gmail.com>
2019-05-08 04:39:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,14 @@
 def oxford_comma(array)
-  string=array.join(", ")
-
-  # array.each do |item|
-  #   if array.length==1
-  #     string+=item
-  #   elsif array.length==2
-  #     string+="#{item} "
-  #   elsif array.last==item
-  #     string+=", and #{item}"
-  #   else
-  #     string+=", #{item}"
-  #   end
-  # end
+  array.each do |item|
+    if array.length==1
+      string+=item
+    elsif array.length==2
+      string+="#{item} "
+    elsif array.last==item
+      string+=", and #{item}"
+    else
+      string+=", #{item}"
+    end
+  end
   return string
 end
