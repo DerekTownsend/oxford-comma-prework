fda3c856f6758d78a012414d5e1a6db2a1c0a94c
Derek Townsend <derekjavon2@gmail.com>
2019-05-08 04:37:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,16 @@
 def oxford_comma(array)
-  string=""
-  array.each do |item|
-    if array.length==1
-      string+=item
-    elsif array.length==2
-      string+="#{item} "
-    elsif array.last==item
-      string+=", and #{item}"
-    else
-      string+=", #{item}"
-    end
-  end
+  string=array.join(",")
+
+  # array.each do |item|
+  #   if array.length==1
+  #     string+=item
+  #   elsif array.length==2
+  #     string+="#{item} "
+  #   elsif array.last==item
+  #     string+=", and #{item}"
+  #   else
+  #     string+=", #{item}"
+  #   end
+  # end
   return string
 end
